39d6195c9d210f5326694101f6477d542ff94977
Tanush-lol <tanushroy1@gmail.com>
2026-07-22 17:12:15 +0000

    so basically we are making a struct global scoped state our_state which is used to communicate with the compositor

--- wayland/guide.md
+++ wayland/guide.md
@@ -13,3 +13,9 @@ every program does not pop up directly example when discord might not directly s
 so this registry is not an object think of it like 
 struct FILE *fp;
 this is just a protocol to connect to the hyprland
+
+compositor is the program which controls your gui example hyprland,Kwin for kde 
+compositor is responsible for where each window pops out how every window moves and then binds every window into an image which is displayed on the machine, now this compositor is a game engine basically and is also written using famous game engine libraries like vulkan, openGL
+
+- wl_registry_add_listener() 
+this function returns 
--- wayland/w.c
+++ wayland/w.c
@@ -1,38 +1,43 @@
-#include <wayland-client.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <wayland-client.h>
+#include <string.h>
 
-void reg_glob(void *data,
-              struct wl_registry *reg,
-              uint32_t name,
-              const char *interface,
-              uint32_t version)
-{
-    printf("Name: %u Interface: %s Version: %u\n",
-           name,
-           interface,
-           version);
-}
-
-void reg_glob_rem(void *data,struct wl_registry *reg,uint32_t name){
-
-}
-
-struct wl_registry_listener reg_list = {
-  .global = reg_glob,
-  .global_remove = reg_glob_rem,
+struct our_state{
+  struct wl_compositor  *compositor;
 };
 
-int main(){
-  struct wl_display *disp = wl_display_connect(0);
-  if (!disp) {
+    struct our_state state ;
+static void registry_handle_global(void *data, struct wl_registry *registry,uint32_t name, const char *interface,uint32_t version)
+{
+    state.compositor = data;
+    if (strcmp(interface, wl_compositor_interface.name) == 0) {
+        state.compositor = wl_registry_bind(
+            registry, name, &wl_compositor_interface, 4);
+    }
+}
+
+static void registry_handle_global_remove(void *data, struct wl_registry *registry,
+		uint32_t name)
+{
+}
+
+static const struct wl_registry_listener registry_listener = {
+	.global = registry_handle_global,
+	.global_remove = registry_handle_global_remove,
+};
+
+int
+main(int argc, char *argv[])
+{
+	struct wl_display *display = wl_display_connect(0);
+  if (!display) {
     return -1;
   }
-  struct wl_registry *reg = wl_display_get_registry(disp);
-  wl_registry_add_listener(reg, &reg_list,0);
-  wl_display_roundtrip(disp);
+	struct wl_registry *registry = wl_display_get_registry(display);
+  
+	wl_registry_add_listener(registry, &registry_listener, &state);
 
-  wl_display_disconnect(disp);
-  return 0;
+	wl_display_roundtrip(display);
+	return 0;
 }
